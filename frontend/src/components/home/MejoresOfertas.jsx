3b86ec163adb640c7d4404d9747e19a03e260f75
javier huaman <javierantonio_huamannunez@iescarlesvallbona.cat>
2026-04-14 19:37:16 +0200

    fix:nueva seccion MejoresOfertas usando la api CheapShark

--- frontend/src/components/home/MejoresOfertas.jsx
+++ frontend/src/components/home/MejoresOfertas.jsx
@@ -1,1 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { GridJuegos } from "../juegos/GridJuegos";
+
+const MejoresOfertas=()=>{
+    const[juegos,setJuegos]=useState([]);
+    const[cargando,setCargando]=useState(true);
+    useEffect(()=>{
+            fetch("http://localhost:3000/ofertas")
+            .then(res=>res.json())
+            .then(data=>{
+                  const juegosFormateados = data.map((j) => ({
+          id: j.dealID,
+          nombre: j.title,
+          imagen: j.thumb,
+          precio: j.salePrice,
+          descuento: j.savings,
+          tienda: j.storeID,
+        }));
+ setJuegos(juegosFormateados);
+        setCargando(false);
+      })
+      .catch((err) => {
+        console.log("Error:", err);
+        setCargando(false);
+      });
+  }, []);  if (cargando) return <p>Cargando ofertas...</p>;
+
+    return(
+        <div className="mejores-ofertas">
+            <h2>Mejores Ofertas</h2>
+            <GridJuegos juegos={juegos} />
+        </div>
+    )
+}
+export default MejoresOfertas;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,51 +1,10 @@
 import GridJuegos from "../components/juegos/GridJuegos";
-
+import MejoresOfertas from "../components/home/MejoresOfertas";
 const Home = () => {
-  const juegosTest = [
-    {
-      id: 1,
-      nombre: "GTA V",
-      imagen: "https://picsum.photos/300/200?1",
-      metacritic: 96,
-      precio: 9.99,
-      descuento: 80,
-      tienda: "1",
-    },
-    {
-      id: 2,
-      nombre: "The Witcher 3",
-      imagen: "https://picsum.photos/300/200?2",
-      metacritic: 92,
-      precio: 7.99,
-      descuento: 70,
-      tienda: "1",
-    },
-    {
-      id: 3,
-      nombre: "Cyberpunk 2077",
-      imagen: "https://picsum.photos/300/200?3",
-      metacritic: 86,
-      precio: 14.99,
-      descuento: 50,
-      tienda: "25",
-    },
-    {
-      id: 4,
-      nombre: "Elden Ring",
-      imagen: "https://picsum.photos/300/200?4",
-      metacritic: 95,
-      precio: 39.99,
-      descuento: 20,
-      tienda: "1",
-    },
-    
-  ];
-
+  
   return (
     <div className="container">
-      <h2>Test Grid</h2>
-
-      <GridJuegos juegos={juegosTest} />
+      <MejoresOfertas />
     </div>
   );
 };
